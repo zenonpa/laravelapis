8ca64e6aad8150e36ccfcb4d4d7363d44013dbdf
Zenon Paredes <zenonpa@gmail.com>
2021-05-07 23:00:45 -0500

    T2.01

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Laravel Apis</title>
+    
+<!-- Material Design for Bootstrap fonts and icons -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+<!-- Material Design for Bootstrap CSS -->
+<link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+<link rel="stylesheet" href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css"/> 
+
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+    }
+</style>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,19 @@
+<!-- <div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="/">SmartChef</a>
+        <ul class="nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </div>
+</div> -->
+
+<div class="mdc-touch-target-wrapper">
+            <button class="mdc-button mdc-button--touch">
+                <span class="mdc-button__ripple"></span>
+                <span class="mdc-button__label">Michelles Menu</span>
+                <span class="mdc-button__touch"></span>
+            </button>
+</div>
